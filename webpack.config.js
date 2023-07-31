d5b1845686592e5326047da3039fc6feaa614d72
hermanzuiakov <herman.zuiakov@gmail.com>
2023-07-31 13:41:22 +0200

    Developing starter template using webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, "src/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "js/[name][contenthash].js",
+        clean: true,
+        assetModuleFilename: "assets/[name][ext]",
+    },
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Starter Template",
+            filename: "index.html",
+            template: "src/template.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/assets", to: "assets" },
+            ],
+        }),
+        new BundleAnalyzerPlugin(),
+    ],
+};
